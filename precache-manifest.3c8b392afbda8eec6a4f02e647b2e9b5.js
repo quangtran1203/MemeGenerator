5d08bdab11482ea7ed37b3a6a483519ddfa5d469
quangtran1203 <harrytran1232001@gmail.com>
2020-10-21 16:06:15 -0400

    Updates

---
+++ precache-manifest.3c8b392afbda8eec6a4f02e647b2e9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d46ba67b03264532f8ef5d0f8a620c4",
+    "url": "/MemeGenerator/index.html"
+  },
+  {
+    "revision": "74362a411300750f2d37",
+    "url": "/MemeGenerator/static/css/main.47dc1667.chunk.css"
+  },
+  {
+    "revision": "9793706f2b2e91e7ae16",
+    "url": "/MemeGenerator/static/js/2.36c45c97.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/MemeGenerator/static/js/2.36c45c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74362a411300750f2d37",
+    "url": "/MemeGenerator/static/js/main.e3aaa087.chunk.js"
+  },
+  {
+    "revision": "cd11528023e4b2c04601",
+    "url": "/MemeGenerator/static/js/runtime-main.68a53f80.js"
+  }
+]);
